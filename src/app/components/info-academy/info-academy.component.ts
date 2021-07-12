aec9a376cf91a20fb5976429827e7e6c103b941d
Niverdrm <niverdromero12@gmail.com>
2021-07-12 11:51:22 -0500

    mejoras

--- src/app/components/info-academy/info-academy.component.ts
+++ src/app/components/info-academy/info-academy.component.ts
@@ -21,26 +21,25 @@ export class InfoAcademyComponent implements OnInit, OnDestroy {
   subscription: Subscription = new Subscription();
 
   constructor(
-    private uiService: UiService, 
+    private uiService: UiService,
     private store: Store<AppState>,
     private router: Router
-    ) {
+  ) {
     this.uiService.updateTitleNavbar('Perfil');
   }
- 
 
   ngOnInit(): void {
-      this.subscription = this.store
-      .pipe( 
-        map(({auth}) =>({auth})),
-        filter(({auth}) => auth.user !== null)
-      )
-      .subscribe(({auth:{user}}) =>{
-        this.user = user; 
-      } );
+    this.subscription = this.store
+      .pipe(filter(({ auth }) => auth.user !== null))
+      .subscribe(({ auth: { user }, ui: { userActive } }) =>
+        user.rol === 'estudiante'
+          ? (this.user = user)
+          : (this.user = userActive)
+      );
   }
+
   onNavigateToCourseData() {
-      this.router.navigate(['/vicerrector/datos-curso']);
+    this.router.navigate(['/vicerrector/datos-curso']);
   }
 
   ngOnDestroy(): void {
