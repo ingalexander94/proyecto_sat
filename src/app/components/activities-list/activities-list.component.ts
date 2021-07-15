4f50414122ecfdbec84916807d96e2025e136eb7
Niverdrm <niverdromero12@gmail.com>
2021-07-15 15:43:53 -0500

    hola

--- src/app/components/activities-list/activities-list.component.ts
+++ src/app/components/activities-list/activities-list.component.ts
@@ -1,20 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { activities } from 'src/app/model/data';
-import {ActivitiesList}  from 'src/app/model/ui';
+import { ActivitiesList } from 'src/app/model/ui';
 import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-activities-list',
   templateUrl: './activities-list.component.html',
-  styleUrls: ['./activities-list.component.css']
+  styleUrls: ['./activities-list.component.css'],
 })
 export class ActivitiesListComponent implements OnInit {
-   activitiesList: ActivitiesList[]=activities;
-  constructor(private  uiService:UiService) { 
-   this.uiService.updateTitleNavbar("Actividades")
+  activitiesList: ActivitiesList[] = activities;
+  constructor(private uiService: UiService) {
+    this.uiService.updateTitleNavbar('Actividades');
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
